62c6700fd8fa1c2ddd2e60d21f70aea2ae028ac5
Atharva S. Kokane <atharvashashankkokane@gmail.com>
2026-02-11 23:11:59 -0500

    Update resume page with new header structure

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -13,6 +13,7 @@ export default async function ResumePage() {
   return (
     <div className="space-y-24">
       <header className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-6 animate-fade-in-up">
+        <div className="w-12 h-px bg-border mb-8" />
         <h1 className="text-4xl font-normal text-white tracking-tight">resume</h1>
         <a
           href="https://drive.google.com/file/d/1fIKJW-9u3C9KU7Y329AzDK8OmH6V9OTS/view?usp=drive_link"
